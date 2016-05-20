511435e517caff283ae3806e32cd3ed73d9a1ad3
Nikki K <808nikz@gmail.com>
2016-05-20 00:01:16 -1000

    Forgot to commit

--- secret_number.js
+++ secret_number.js
@@ -1,9 +1,14 @@
 
 'use strict';
 module.exports = function() {
-
+	//Creates a function that returns the same random number when invoked.
+	//Creates a random number
+	//Save private local number to 'secretNum'
   var secretNum = Math.floor(Math.random()*100001);
+
+  //Gets the saved random number from the closure.
   return function(){
+  	//Nested local scope
   	return secretNum;
   };
   
